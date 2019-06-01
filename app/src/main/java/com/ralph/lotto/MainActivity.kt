d5654eb123370a9e142dcbf8b370fe490869d2ea
ralphjy78 <ralphjy@gmail.com>
2019-06-01 18:32:31 +0900

    랜덤으로 번호 선정

--- app/src/main/java/com/ralph/lotto/MainActivity.kt
+++ app/src/main/java/com/ralph/lotto/MainActivity.kt
@@ -5,6 +5,7 @@ import android.support.v7.app.AppCompatActivity
 import android.os.Bundle
 import android.widget.Toast
 import kotlinx.android.synthetic.main.activity_main.*
+import java.util.*
 
 class MainActivity : AppCompatActivity() {
 
@@ -15,7 +16,12 @@ class MainActivity : AppCompatActivity() {
         Toast.makeText(applicationContext, "MainActivity 입니다.", Toast.LENGTH_LONG).show()
 
         randomCard.setOnClickListener {
-            startActivity(Intent(this, ResultActivity::class.java))
+
+            // 랜덤으로 생성된 6개 수를 해당 액티비티로 보냄
+            val intent = Intent(this, ResultActivity::class.java)
+            intent.putIntegerArrayListExtra("result", ArrayList(getShuffleLottoNumbers()/*getRandomLottoNumybers()*/))
+
+            startActivity(intent)
         }
 
         constellationCard.setOnClickListener {
@@ -27,4 +33,47 @@ class MainActivity : AppCompatActivity() {
         }
 
     }
+
+    /**
+     * Shuffle로 무작위 로또 번호 생성하기
+     */
+    fun getShuffleLottoNumbers(): MutableList<Int> {
+        val list = mutableListOf<Int>()
+
+        for(i in 1..45) {
+            list.add(i)
+        }
+
+        list.shuffle()      // 리스트를 마구 섞는다
+
+        return list.subList(0, 6).sorted().toMutableList()
+    }
 }
+    /**
+     * 랜덤으로 1~45 수를 선택하는 함수
+     */
+    inline fun getRandomLottoNumber(): Int {
+       return Random().nextInt(45) + 1
+    }
+
+    /**
+     * 랜덤으로 추출된 수를 6개 만드는 함수
+     */
+    fun getRandomLottoNumybers(): MutableList<Int> {
+        val lottoNumbers = mutableListOf<Int>()
+
+        var randomNumber = 0
+
+        for(i in 1..6) {
+            do {
+                randomNumber = getRandomLottoNumber()
+
+            } while (lottoNumbers.contains(randomNumber))
+
+            lottoNumbers.add(randomNumber)
+        }
+
+        return lottoNumbers
+    }
+
+
--- app/src/main/java/com/ralph/lotto/ResultActivity.kt
+++ app/src/main/java/com/ralph/lotto/ResultActivity.kt
@@ -3,6 +3,7 @@ package com.ralph.lotto
 import android.support.v7.app.AppCompatActivity
 import android.os.Bundle
 import android.widget.Toast
+import kotlinx.android.synthetic.main.activity_result.*
 
 class ResultActivity : AppCompatActivity() {
 
@@ -12,5 +13,26 @@ class ResultActivity : AppCompatActivity() {
 
         Toast.makeText(applicationContext, "ResultActivity 입니다.", Toast.LENGTH_LONG).show()
 
+        // 앞 화면에서 전달받은 랜덤 숫자 6개를 인텐트로부터 꺼내온다
+        val result = intent.getIntegerArrayListExtra("result")
+
+        result?.let {
+            //updateLottoBallImage(ArrayList(result.sorted()))
+            updateLottoBallImage(ArrayList(result))
+        }
+    }
+
+    // 로또 1번 공 이미지
+    val lottoImageStartId = R.drawable.ball_01
+
+    fun updateLottoBallImage(result: ArrayList<Int>) {
+        if(result.size < 6) return
+
+        imageView1.setImageResource(lottoImageStartId + (result[0] - 1))
+        imageView2.setImageResource(lottoImageStartId + (result[1] - 1))
+        imageView3.setImageResource(lottoImageStartId + (result[2] - 1))
+        imageView4.setImageResource(lottoImageStartId + (result[3] - 1))
+        imageView5.setImageResource(lottoImageStartId + (result[4] - 1))
+        imageView6.setImageResource(lottoImageStartId + (result[5] - 1))
     }
 }
